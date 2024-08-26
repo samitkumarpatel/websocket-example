1590439268f69fbc1b690739e76dd10598ccef31
samitkumarpatel <samitkumarpatel@gmail.com>
2024-08-26 22:00:03 +0200

    UI impl

--- src/main/java/net/samitkumar/websocket_example/MessageController.java
+++ src/main/java/net/samitkumar/websocket_example/MessageController.java
@@ -1,18 +1,21 @@
 package net.samitkumar.websocket_example;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MessageController {
 
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/messages")
     public void sendMessageToUser(UserMessage userMessage) {
+        log.info("{}", userMessage);
         messagingTemplate.convertAndSendToUser(userMessage.userId(), "/queue/messages", userMessage.message());
     }
 }
